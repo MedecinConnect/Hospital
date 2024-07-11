3459a3219f1d6474fc8854e36cc6490f179e9145
sami el <s.elidrissidafali@elaraki.ac.ma>
2024-07-11 12:52:12 -0400

    project-setup +taliwind css classes

---
+++ frontend/src/layout/Layout.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Header from "../componnents/Header/Header";
+import Footer from "../componnents/Footer/Footer";
+import Router from "../routes/Router";
+
+
+
+const Layout = () => {
+  return <>
+  <Header/>
+  <main>
+    <Router/>
+  </main>
+  <Footer/>
+  
+  </>
+}
+export default Layout
