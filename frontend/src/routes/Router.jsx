926cba538da1d8eac85d73c5600558513b4b2fc8
sami el <s.elidrissidafali@elaraki.ac.ma>
2024-07-15 21:19:55 -0400

    AJout de la recherche de docteurs

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -1,10 +1,9 @@
 import Home from "../pages/Home";
 import Services from "../pages/Services";
 import Login from "../pages/Login";
-import Signup from "../pages/Signup";
+import Register from "../pages/Register";
 import Contact from "../pages/Contact";
 import Doctors from "../pages/Doctors/Doctors";
-import Register from "../pages/Register";
 import DoctorDetails from "../pages/Doctors/DoctorDetails";
 import {Routes,Route} from 'react-router-dom';
 
@@ -17,7 +16,6 @@ const Router = () => {
     <Route path="/doctors/:id" element={<DoctorDetails/>}/>
     <Route path="/login" element={<Login/>}/>
     <Route path="/register" element={<Register/>}/>
-    <Route path="/signup" element={<Signup/>}/>
 
     <Route path="/contact" element={<Contact/>}/>
     <Route path="/services" element={<Services/>}/>
