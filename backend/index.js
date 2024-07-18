1b45a00c93d6d9595f59f553d67bae075f54bec1
sami el <s.elidrissidafali@elaraki.ac.ma>
2024-07-18 16:47:03 -0400

    connection a la base de donnes

--- backend/index.js
+++ backend/index.js
@@ -13,14 +13,29 @@ const corsOptions = {
   origin: true
 };
 
-app.use(cors(corsOptions));
-app.use(cookieParser());
-app.use(express.json());
-
 app.get('/', (req, res) => {
   res.send('API is listening');
 });
 
+// Database connection
+mongoose.set('strictQuery', false);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+
+    console.log('MongoDB is connected');
+  } catch (error) {
+    console.error('MongoDB connection failed:', error.message);
+    process.exit(1); // Exit the process with a failure code
+  }
+};
+
+// Middleware
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.json());
+
 app.listen(port, () => {
+  connectDB();
   console.log(`Server is listening on port ${port}`);
 });
